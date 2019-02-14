3fd98debcb1a4d9cbe0d8785d55c04eb28159fb4
sardylan <sardylan@gmail.com>
2019-02-14 12:40:47 +0100

    Adds git branch utility

---
+++ profile.d/90-git_branch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+parse_git_branch() {
+    if [ -d .git ]; then
+        echo -ne "\e[95m{"
+        git branch 2> /dev/null | sed -e "/^ /d" -e "s/* //g" | tr -d "\n"
+        echo -ne "}\e[39m "
+    fi
+}
+
+PS1="\$(parse_git_branch)${PS1}"
+
+export PS1
+
